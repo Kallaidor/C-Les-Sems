9cfc9f95642abb13a41e6e14e15173a2e175ba12
Kallaidor <Signalup3000@tandex.ru>
2022-07-06 16:17:47 +0300

    добавил DZ2 и DZ3

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+Console.WriteLine("введите цифру дня недели");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && x < 6)
+{
+    Console.Write("Нет");
+}
+
+if (x > 5 && x < 8)
+{
+    Console.Write("Да");
+}
+
+if (x < 1 || x > 7)
+{
+    Console.Write("Ввели не корректный день недели");
+}
+*/
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+Console.WriteLine("введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x < 100)
+{
+    Console.Write("третьей цифры нет");
+}
+else
+{
+    while (x >= 1000)
+    {
+        x = x / 10;
+    }
+
+    if (x < 1000)
+    {
+        int three = x % 100;
+        int tow = three % 10;
+        Console.Write(tow);
+    }
+}
+*/
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+Console.WriteLine("введите трехзначное число");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x < 1000 && x > 99)
+{
+int two = x / 10;
+int result = two % 10;
+Console.Write(result);
+}
+else
+Console.WriteLine("ввели не трехзначное число");
+*/
+// Доп. задача 16: Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+/*
+Console.WriteLine("введите число 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int quadro1 = Quadro(num1);
+int quadro2 = Quadro(num2);
+
+if (quadro1 == num2 || quadro2 == num1)
+{
+Console.Write("Да");
+}
+else 
+{
+  Console.Write("Нет");  
+}
+
+int Quadro (int a)
+{
+    return a * a;
+}
+*/
--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+/*
+Console.WriteLine("введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int dig1 = number / 10000;
+int dig2 = (number / 1000) % 10;
+int dig4 = (number / 10) % 10;
+int dig5 = number % 10;
+
+if (number < 9999 || number > 99999)
+{
+    Console.Write("Число не пятизначное");
+}
+else
+{
+    if (dig1 == dig5 && dig2 == dig4)
+    {
+        Console.Write("Да, число - полиндром");
+    }
+    else
+    {
+        Console.Write("Нет, число - не полиндром");
+    }
+}
+*/
+
+//Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+/*
+Console.Write("введите координату точки A по x: ");
+double aX = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите координату точки A по y: ");
+double aY = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите координату точки A по z: ");
+double aZ = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введите координату точки B по x: ");
+double bX = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите координату точки B по y: ");
+double bY = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите координату точки B по z: ");
+double bZ = Convert.ToInt32(Console.ReadLine());
+
+double quadroX = Coord(bX, aX);
+double quadroY = Coord(bY, aY);
+double quadroZ = Coord(bZ, aZ);
+double result = Math.Sqrt(quadroX + quadroY + quadroZ);
+
+double Coord(double a, double b)
+{
+    return Math.Pow((b - a), 2);
+}
+
+Console.WriteLine($"расстояние между точками = {result}");
+*/
+//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*
+Console.WriteLine("Введите число N");
+double num = Convert.ToInt32(Console.ReadLine());
+double numCube = Math.Pow(num, 3);
+double count = 1;
+double Cube = 0;
+
+while (count < num)
+{
+    Cube = Math.Pow(count, 3);
+    Console.Write(Cube + ",");
+    count++;
+}
+Console.Write(numCube);
+*/
