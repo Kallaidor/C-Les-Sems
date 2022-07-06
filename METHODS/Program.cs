8a342cdcdb029a7e0bf7c1218c482f2e1e0e96e8
Kallaidor <Signalup3000@tandex.ru>
2022-07-06 12:30:23 +0300

    new files

---
+++ METHODS/Program.cs
@@ -0,0 +1,22 @@
+﻿//Вывод одномерного массива
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+
+//заполнение одномерного массива случайными числами от 100 до 999
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+
+
