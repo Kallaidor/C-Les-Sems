e174469c0a1e06f71a603385d006125194bf3b34
Kallaidor <Signalup3000@tandex.ru>
2022-07-08 18:04:00 +0300

    добавил DZ

--- DZ8/Program.cs
+++ DZ8/Program.cs
@@ -1,2 +1,203 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Задача 62: Заполните спирально массив 4 на 4.
+*/
+
+//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+/*
+int linesVol = Input("Введите количество строк: ");
+int columnsVol = Input("Введите количество столбцов: ");
+
+int[,] array = new int[linesVol, columnsVol];
+FillArrayRandomNumbers(array);
+Console.WriteLine("наш Массив");
+PrintArray(array);
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+    {
+        for (int k = 0; k < array.GetLength(1) - 1; k++)
+        {
+            if (array[i, k] < array[i, k + 1])
+            {
+                int temp = 0;
+                temp = array[i, k];
+                array[i, k] = array[i, k + 1];
+                array[i, k + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine("Упорядоченный массив");
+PrintArray(array);
+
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArrayRandomNumbers(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+            numbers[i, j] = new Random().Next(0, 100);
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine();
+    }
+
+}
+*/
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+/*
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+Console.WriteLine("введите размер прямоугольного массива");
+int massVol = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[massVol, massVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int sum = 0;
+int minsum = int.MaxValue;
+int indexMinLine = 0;
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    sum =0;
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        sum = sum + numbers[i, j];
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        indexMinLine = i+1;
+    }
+    Console.WriteLine($"сумма {i+1}-ой строки равна {sum}");
+}
+
+Console.WriteLine($"строка с наименьшей суммой элементов под номером -  {indexMinLine}");
+*/
+
+//Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+
+
+void Fill3DArrayRandomNumbers(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(0, 100);
+                for (int x = 0; x < i; x++)
+                {
+                    for (int y = 0; y < j; y++)
+                    {
+                        for (int z = 0; z < k; z++)
+                        {
+                            while (array[i, j, k] == array[x, y, z])
+                               Fill3DArrayRandomNumbers(array);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("[ ");
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write(array[i, j, k] + " ");
+            }
+            Console.Write("]");
+            Console.WriteLine("");
+        }
+        Console.WriteLine("");
+    }
+}
+
+int[,,] numbers = new int[2, 3, 4];
+Fill3DArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+/*
+int[] mass = new int[5];
+FillArrayRandomNumbers(mass);
+PrintArray(mass);
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+        for (int j = 0; j < i; j++)
+        {
+            while (array[i] == array [j])
+            FillArrayRandomNumbers(array);
+        }
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine("");
+}
+*/
--- sem10/Program.cs
+++ sem10/Program.cs
@@ -1,2 +1,200 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 70: Напишите программу, которая на вход принимает два числа и выдаёт
+первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+3 и 4, N = 5 -> 3 4 7 11 18
+6 и 10, N = 4 -> 6 10 16 26
+*/
+
+/*
+int Input(string output)
+{
+Console.Write(output);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int x = Input("введите Х: ");
+int y = Input("введите y: ");
+int n = Input("введите количество чисел: ");
+
+for (int i = 1; i <= n; i++)
+{
+Console.Write($"{NaturalNumber(x, y, i)} ");
+
+}
+
+
+int NaturalNumber(int x, int y, int n)
+{
+if (n == 1)
+return x;
+if (n == 2)
+return y;
+else
+{
+return NaturalNumber(x, y, n - 1) + NaturalNumber(x, y, n - 2);
+}
+}
+*/
+/*
+int first = 3;
+int second = 4;
+int countNum = 5;
+Console.WriteLine(Fib(first,second,countNum));
+
+string Fib(int first, int second, int countNum)
+{
+if(countNum == 0)
+{
+return string.Empty;
+}
+else
+{
+return first + " " + Fib (second, first + second, —countNum);
+}
+}
+*/
+
+/*
+Задача 71: В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в».
+Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита.
+n = 2 -> аа, ии, сс, вв, аи, иа, ис, си, ас, са,
+ав, ва, ви, ив, св, вс
+*/
+/*
+string charsWords = "аисв";
+int countCharsInWords = 3;
+PrintAllWords(charsWords,countCharsInWords,"");
+
+void PrintAllWords(string alphabet, int length, string prefix)
+{
+if (length == 0)
+{
+Console.Write(prefix + " ");
+}
+else
+{
+for (int i = 0; i < alphabet.Length; i++)
+{
+PrintAllWords(alphabet,length-1,prefix + alphabet[i]);
+}
+}
+}
+*/
+/*
+string alphabet = "аисв";
+int n = 2;
+
+void FindWords(string alph, char[] word, int length = 0)
+{
+if (length == word.Length)
+{
+Console.Write(new String(word)+ " ");
+}
+else
+{
+for (int i = 0; i < alph.Length; i++)
+{
+word[length] = alph[i];
+FindWords(alph, word, length + 1);
+}
+}
+}
+
+FindWords(alphabet, new char[n]);
+*/
+/*
+Задача 72: Заданы 2 массива: info и data.
+В массиве info хранятся двоичные представления нескольких чисел (без разделителя).
+В массиве data хранится информация о количестве бит, которые занимают числа из массива info. Напишите программу, которая составит массив десятичных представлений чисел массива data с учётом информации из массива info.
+входные данные:
+data = {0, 1, 1, 1, 1, 0, 0, 0, 1 }
+info = {2, 3, 3, 1 }
+выходные данные:
+1, 7, 0, 1
+*/
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+
+
+int[] data = { 0, 1, 1, 1, 1, 0, 0, 0, 1 };
+int[] info = { 2, 3, 3, 1 };
+int dec = 0;
+int index = 0;
+PrintArray(data);
+Console.WriteLine();
+PrintArray(info);
+Console.WriteLine();
+
+for (int i = 0; i < info.Length; i++)
+{
+    dec = 0;
+    for (int j = 0; j < info[i]; j++)
+    {
+        dec += Convert.ToInt32(Math.Pow(2, j)) * data[index + info[i] - j - 1];
+    }
+    index = index + info[i];
+    Console.Write($"{dec} ");
+}
+
+
+/*
+int[] data = { 0, 1, 1, 1, 1, 0, 0, 0, 1 };
+int[] info = { 2, 3, 3, 1 };
+
+int[] decimalNumbers = GetNumbersFromDataInfo(data, info);
+
+Console.Write("data = ");
+PrintArray(data);
+
+Console.Write("info = ");
+PrintArray(info);
+
+Console.Write("result = ");
+PrintArray(decimalNumbers);
+
+
+int GetDecimal(int[] bits, int minIdx, int maxIdx)
+{
+int number = 0;
+for (int i = minIdx; i < maxIdx + 1; i++)
+{
+number += bits[i] * (int)Math.Pow(2, maxIdx - i);
+}
+return number;
+}
+
+int[] GetNumbersFromDataInfo(int[] bits, int[] sizes)
+{
+int[] numbers = new int[sizes.Length];
+
+int startIdx = 0;
+int endIdx = -1;
+for (int i = 0; i < numbers.Length; i++)
+{
+startIdx = endIdx + 1;
+endIdx = endIdx + sizes[i];
+int number = GetDecimal(bits, startIdx, endIdx);
+numbers[i] = number;
+}
+
+return numbers;
+}
+
+void PrintArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+}
+
+*/
